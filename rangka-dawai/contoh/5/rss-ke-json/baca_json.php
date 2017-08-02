4b9e2d9496481462753954412cf1d632e576c285
amin007 <helmi007@yahoo.com>
2017-08-02 09:15:29 +0800

    cuba dapatkan element dalam jadual waktu solat

--- rangka-dawai/contoh/5/rss-ke-json/baca_json.php
+++ rangka-dawai/contoh/5/rss-ke-json/baca_json.php
@@ -1,36 +1,39 @@
 <?php
 $setahun = 'http://www.e-solat.gov.my/web/waktusolat.php?zone=JHR04&state=JOHOR&year=2017&jenis=year&bulan=08&LG=BM';
-$file = file_get_contents($setahun);
-//echo '<pre>'; //print_r($file);
-//*/
+//$htmlContent = file_get_contents("http://teskusman.esy.es/index.html");
+$htmlContent = file_get_contents($setahun);
+		
+$DOM = new DOMDocument();
+@$DOM->loadHTML($htmlContent);
+	
+$Header = $DOM->getElementsByTagName('th');
+//$Detail = $DOM->getElementsByTagName('td');
+$Detail = $DOM->getElementsByTagName('font');
 
-$pecahbelah = explode('</tr', $file); 
-//echo '<pre>'; print_r($pecah);
-//$senaraiKey = array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19);
-$senaraiKey = array(0,1,2,3,4,5,6,7,8,9,10,42,43,44,45,46,47,48,49);
-foreach($pecahbelah as $key =>  $data)
+/*#Get header name of the table
+foreach($Header as $NodeHeader) 
 {
-	if ( in_array($key,$senaraiKey) )
-		echo '';
-	else
-	{	//echo '<hr>$key => ' . $key . ' | ' . $data;
-		//$datakecil[$key][] = explode('</td', $data); 
-		$datakecil[$key][] = explode('</font', $data); 
-	}	
+	$aDataTableHeaderHTML[] = trim($NodeHeader->textContent);
+}//echo '<pre>';print_r($aDataTableHeaderHTML);//*/
+
+//#Get row data/detail table without header name as key
+$i = $j = 0;
+foreach($Detail as $sNodeDetail) 
+{
+	$aDataTableDetailHTML[$j][] = trim($sNodeDetail->textContent);
+	$i = $i + 1;
+	@$j = $i % count($aDataTableHeaderHTML) == 0 ? $j + 1 : $j;
+}echo '<pre>'; print_r($aDataTableDetailHTML);
+
+/*#Get row data/detail table with header name as key and outer array index as row number
+for($i = 0; $i < count($aDataTableDetailHTML); $i++)
+{
+	for($j = 0; $j < count($aDataTableHeaderHTML); $j++)
+	{
+		$aTempData[$i][$aDataTableHeaderHTML[$j]] = $aDataTableDetailHTML[$i][$j];
+	}
 }
+$aDataTableDetailHTML = $aTempData; unset($aTempData);
 
-echo '<pre>'; print_r($datakecil);
-
-/*
-libxml_use_internal_errors(true);
-$doc = new DOMDocument;
-$doc->loadHTML($file); // print_r($doc);
-$xpath = new DOMXpath($doc); print_r($xpath);
-
-/*
-// A name attribute on a <td>
-// $xpath->query( '//td')->item(0);
-$values = $xpath->query('//td');
-
+echo '<pre>'; print_r($aDataTableDetailHTML);
 //*/
-
