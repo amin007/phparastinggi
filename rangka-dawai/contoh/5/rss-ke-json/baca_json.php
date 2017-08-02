39484255892bc45bd320455f54252efb772cd3e3
amin007 <helmi007@yahoo.com>
2017-08-02 14:25:24 +0800

    asingkan tajuk dan data jadual waktu jakim
    mungkin kena ubah setiap tahun

--- rangka-dawai/contoh/5/rss-ke-json/baca_json.php
+++ rangka-dawai/contoh/5/rss-ke-json/baca_json.php
@@ -1,39 +1,44 @@
 <?php
+
+# https://www.codeproject.com/Tips/1074174/Simple-Way-to-Convert-HTML-Table-Data-into-PHP-Arr
 $setahun = 'http://www.e-solat.gov.my/web/waktusolat.php?zone=JHR04&state=JOHOR&year=2017&jenis=year&bulan=08&LG=BM';
 //$htmlContent = file_get_contents("http://teskusman.esy.es/index.html");
 $htmlContent = file_get_contents($setahun);
-		
+
 $DOM = new DOMDocument();
 @$DOM->loadHTML($htmlContent);
-	
-$Header = $DOM->getElementsByTagName('th');
+
+//$Header = $DOM->getElementsByTagName('td');
 //$Detail = $DOM->getElementsByTagName('td');
 $Detail = $DOM->getElementsByTagName('font');
 
-/*#Get header name of the table
-foreach($Header as $NodeHeader) 
-{
-	$aDataTableHeaderHTML[] = trim($NodeHeader->textContent);
-}//echo '<pre>';print_r($aDataTableHeaderHTML);//*/
-
-//#Get row data/detail table without header name as key
+# Get row data/detail table without header name as key
+$senaraiKey = array(0,1,2,3,4,5,6,7,8,9,10,11,12);
+$senaraiTajuk = array('Tarikh','Hari','Imsak','Subuh','Syuruk','Zohor','Asar','Maghrib','Isyak');
+$buangData = array('Januari','Februari','Mac',
+'April','Mei','Jun',
+'Julai','Ogos','September',
+'Oktober', 'November', 'Disember',
+'Disember 2016','Januari 2018');
 $i = $j = 0;
-foreach($Detail as $sNodeDetail) 
-{
-	$aDataTableDetailHTML[$j][] = trim($sNodeDetail->textContent);
-	$i = $i + 1;
-	@$j = $i % count($aDataTableHeaderHTML) == 0 ? $j + 1 : $j;
-}echo '<pre>'; print_r($aDataTableDetailHTML);
 
-/*#Get row data/detail table with header name as key and outer array index as row number
-for($i = 0; $i < count($aDataTableDetailHTML); $i++)
+foreach($Detail as $kunci => $sNodeDetail) 
 {
-	for($j = 0; $j < count($aDataTableHeaderHTML); $j++)
-	{
-		$aTempData[$i][$aDataTableHeaderHTML[$j]] = $aDataTableDetailHTML[$i][$j];
-	}
+	if ( in_array($kunci,$senaraiKey) ):
+		$tajuk[] = trim(
+			preg_replace('/\s\s+/', ' ', 
+			$sNodeDetail->textContent));
+	else:
+		//echo '<hr>' . $j . '['	. $i++ . ']='
+		//. trim($sNodeDetail->textContent);
+		if ( !in_array(trim($sNodeDetail->textContent),$buangData) )
+			$dataJadual[$j][$senaraiTajuk[$i++]] = trim($sNodeDetail->textContent);
+		$i = ($i == 9) ? 0 : $i++;
+		@$j = $i % count($senaraiTajuk) == 0 ? $j + 1 : $j;
+	endif;
 }
-$aDataTableDetailHTML = $aTempData; unset($aTempData);
+//echo '<pre>tajuk->';print_r($tajuk);
+//echo '<hr>data->';print_r($dataJadual);
 
-echo '<pre>'; print_r($aDataTableDetailHTML);
-//*/
+echo '<pre>tajuk->' . json_encode($tajuk);
+echo '<hr>data->' . json_encode($dataJadual);
